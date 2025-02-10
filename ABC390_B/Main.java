f875df11616887a42ff1b0638f0cdad8ff5d2722
takuya <hand.0126takuya@gmail.com>
2025-02-10 23:12:26 +0900

    2025/02/10

---
+++ ABC390_B/Main.java
@@ -0,0 +1,26 @@
+// package ABC390_B;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+
+    long[] A = new long[N];
+    for (int i = 0; i < N; i++) {
+      A[i] = sc.nextInt();
+    }
+    sc.close();
+
+    String result = "Yes";
+    for (int i = 1; i < N; i++) {
+      if (A[1] * A[i-1] != A[0] * A[i]) {
+        result = "No";
+        break;
+      }
+    }
+
+    System.out.println(result);
+  }
+}
