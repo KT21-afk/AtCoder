b5ba494adf56a585558d8cf918b183d8a26cd965
takuya <hand.0126takuya@gmail.com>
2025-02-14 22:39:59 +0900

    abc389 c

---
+++ ABC389/MainB.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MainB {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    long X = sc.nextLong();
+    sc.close();
+
+    int N = 0;
+    for (int i = 1; 1 < X; i++) {
+      X /= i;
+      N++;
+    }
+    System.out.println(N);
+  }
+}
